39ad4503165be38d691fcf69e5fe46d6ee5cdd7f
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2024-12-05 18:20:59 +0900

    feat: utility 모듈 기본 구조 설정

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,10 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { initializeTransactionalContext } from 'typeorm-transactional';
 
 import { AppModule } from '@api/app.module';
 
-import { Environ } from '@common/environ';
+import { Environ, GlobalExceptionFilter, TrimPipe } from '@common/util';
 
 (async () => {
   initializeTransactionalContext();
@@ -11,6 +12,11 @@ import { Environ } from '@common/environ';
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
+  app.useGlobalFilters(new GlobalExceptionFilter());
+  app.useGlobalPipes(
+    new TrimPipe(),
+    new ValidationPipe({ transform: true, whitelist: true, forbidNonWhitelisted: true }),
+  );
 
   await app.listen(Environ.SERVER_PORT, () => {
     console.log(`======= NODE ENV: ${Environ.NODE_ENV} =======`);
